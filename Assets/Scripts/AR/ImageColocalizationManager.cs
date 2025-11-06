da3c89cbcc2ce1118b69e79e1b984bbf2eeefafb
boymin <sonhyunmin00@naver.com>
2025-11-06 20:27:53 +0900

    feat: StartSharedSpace 함수 작성

--- Assets/Scripts/AR/ImageColocalizationManager.cs
+++ Assets/Scripts/AR/ImageColocalizationManager.cs
@@ -1,16 +1,34 @@
+using Niantic.Lightship.SharedAR.Colocalization;
 using UnityEngine;
 
-public class ImageColocalizationManager : MonoBehaviour
+namespace CAU4R.Tuho.AR
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ImageColocalizationManager : MonoBehaviour
     {
+        [SerializeField]
+        private SharedSpaceManager _sharedSpaceManager;
         
-    }
+        [SerializeField]
+        private Texture2D _targetImage;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField]
+        private float _targetImageSize = 0.1f;
+
+        [SerializeField]
+        private string _roomName = "DemoRoom";
+
+        [SerializeField]
+        private int _roomCapacity = 10;
         
+        [SerializeField]
+        private string _roomDescription = "Demo Room Description";
+
+        public void StartSharedSpace()
+        {
+            var imageTrackingOptions = ISharedSpaceTrackingOptions.CreateImageTrackingOptions(_targetImage, _targetImageSize);
+            var roomOptions = ISharedSpaceRoomOptions.CreateLightshipRoomOptions(_roomName, _roomCapacity, _roomDescription);
+            
+            _sharedSpaceManager.StartSharedSpace(imageTrackingOptions, roomOptions);
+        }
     }
 }
