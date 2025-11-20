334fd9f98c1e9e4dadb67044211d5d28a4dc30b3
boymin <sonhyunmin00@naver.com>
2025-11-20 21:14:02 +0900

    feat: Blit 함수 작성

--- Assets/Scripts/AR/ImageBlit.cs
+++ Assets/Scripts/AR/ImageBlit.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR.ARFoundation;
 
-public class ImageBlit : MonoBehaviour
+namespace CAU4R.Tuho.AR
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ImageBlit : MonoBehaviour
     {
-        
-    }
+        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField]
+        private RenderTexture _renderTexture;
         
+        [SerializeField]
+        private ARCameraBackground _cameraBackground;
+
+        public void Blit()
+        {
+            var commandBuffer = new CommandBuffer();
+            
+            var texture = _cameraBackground.material.HasProperty(MainTex) ? _cameraBackground.material.GetTexture(MainTex) : null;
+            
+            var colorBuffer = Graphics.activeColorBuffer;
+            var depthBuffer = Graphics.activeDepthBuffer;
+            
+            Graphics.SetRenderTarget(_renderTexture);
+            
+            commandBuffer.ClearRenderTarget(true, false, Color.clear);
+            
+            commandBuffer.Blit(
+                texture,
+                BuiltinRenderTextureType.CurrentActive,
+                _cameraBackground.material);
+            
+            Graphics.ExecuteCommandBuffer(commandBuffer);
+            
+            Graphics.SetRenderTarget(colorBuffer, depthBuffer);
+            
+        }
+
+        private void UpdateRenderTextureSize()
+        {
+            var currentWidth = _renderTexture.width;
+            var currentHeight = _renderTexture.height;
+            
+            var newWidth = Screen.width;
+            var newHeight = Screen.height;
+        }
+
+        private void CopyRenderTextureTo2DTexture()
+        {
+            
+        }
     }
 }
