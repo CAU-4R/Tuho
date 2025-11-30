326c510108db18a6533b449ca8cbb117323d78db
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2025-11-30 16:42:51 +0900

    refactor : 게임 캔버스로 점수 관리 로직 이동

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,29 +1,54 @@
 using UnityEngine;
-using TMPro; // TextMeshPro를 사용하기 위해 필요
-using System.Collections; // 코루틴을 사용하기 위해 필요
 
 public class UIManager : MonoBehaviour
 {
-    public TMP_Text scoreText;
-    public GameObject successTextObject;
+    public static UIManager Instance;
 
-    // 점수 텍스트를 업데이트하는 함수
-    public void UpdateScore(int score)
+    public GameObject startCanvas;
+    public GameObject hostCanvas;
+    public GameObject clientCanvas;
+    public GameObject gameCanvas;
+
+    void Awake()
     {
-        scoreText.text = "Score: " + score;
+        // 싱글톤 설정
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
 
-    // 성공 메시지를 보여주는 함수
-    public void ShowSuccessMessage()
+    void Start()
     {
-        StartCoroutine(ShowAndHideSuccessText());
+        ShowStart();
     }
 
-    // 메시지를 1.5초 동안 보여주고 자동으로 숨기는 코루틴
-    private IEnumerator ShowAndHideSuccessText()
+    public void ShowStart()
     {
-        successTextObject.SetActive(true); // 메시지 켜기
-        yield return new WaitForSeconds(1.5f); // 1.5초 기다리기
-        successTextObject.SetActive(false); // 메시지 끄기
+        SetOnly(startCanvas);
+    }
+
+    public void ShowHost()
+    {
+        SetOnly(hostCanvas);
+    }
+
+    public void ShowClient()
+    {
+        SetOnly(clientCanvas);
+    }
+
+    public void ShowGame()
+    {
+        SetOnly(gameCanvas);
+    }
+
+    // 하나만 켜고 나머지는 모두 끄기
+    private void SetOnly(GameObject target)
+    {
+        startCanvas.SetActive(false);
+        hostCanvas.SetActive(false);
+        clientCanvas.SetActive(false);
+        gameCanvas.SetActive(false);
+
+        target.SetActive(true);
     }
 }
